92c845a136dfcaf1d17e18edb2ea584bdd244933
msvr <saivivek512@gmail.com>
2016-01-18 20:08:14 +0530

    hey you

---
+++ Cop_Registration/app/src/main/java/com/example/android/cop_registration/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.android.cop_registration;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    Button send;
+    EditText tname;
+    EditText name1;
+    EditText entry1;
+    EditText name2;
+    EditText entry2;
+    EditText name3;
+    EditText entry3;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tname = (EditText) findViewById(R.id.Team);
+        name1 = (EditText) findViewById (R.id.n1);
+        entry1 = (EditText) findViewById(R.id.e1);
+        name2 = (EditText) findViewById (R.id.n2);
+        entry2 = (EditText) findViewById(R.id.e2);
+        name3 = (EditText) findViewById (R.id.n3);
+        entry3 = (EditText) findViewById(R.id.e3);
+
+        send = (Button) findViewById(R.id.send);
+
+        send.setOnClickListener(this);
+
+    }
+
+    public static String gtext (EditText v) {
+        String data =  v.getText().toString() ;
+        return data;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.send:
+
+                Intent intent = new Intent (this ,confirmation.class);
+
+                intent.putExtra("Team-name",gtext(tname));
+                intent.putExtra("user1", gtext(name1));
+                intent.putExtra("user2", gtext(name2));
+                intent.putExtra("user3", gtext(name3));
+
+                intent.putExtra("entry1", gtext(entry1));
+                intent.putExtra("entry2", gtext(entry2));
+                intent.putExtra("entry3", gtext(entry3));
+                startActivity(intent);
+
+                break;
+
+        }
+    }
+
+
+
+
+}
---
+++ Cop_Registration/app/src/main/java/com/example/android/cop_registration/confirmation.java
@@ -0,0 +1,118 @@
+package com.example.android.cop_registration;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class confirmation extends Activity implements View.OnClickListener{
+
+    RequestQueue requestQueue;
+
+
+    Button confirm;
+    TextView team;
+    TextView tn1;
+    TextView te1;
+    TextView tn2;
+    TextView te2;
+    TextView tn3;
+    TextView te3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmation);
+
+        requestQueue = Volley.newRequestQueue(this);
+        team = (TextView) findViewById(R.id.tTeam);
+        tn1 = (TextView) findViewById(R.id.tn1);
+        tn2 = (TextView) findViewById(R.id.tn2);
+        tn3 = (TextView) findViewById(R.id.tn3);
+        te1 = (TextView) findViewById(R.id.te1);
+        te2 = (TextView) findViewById(R.id.te2);
+        te3 = (TextView) findViewById(R.id.te3);
+
+        confirm = (Button) findViewById(R.id.confirm);
+
+        confirm.setOnClickListener(this);
+        Intent intent = getIntent();
+
+        if(null != intent) {
+            team.setText(intent.getStringExtra("Team-name"));
+            tn1.setText(intent.getStringExtra("user1"));
+            tn2.setText(intent.getStringExtra("user2"));
+            tn3.setText(intent.getStringExtra("user3"));
+            te1.setText(intent.getStringExtra("entry1"));
+            te2.setText(intent.getStringExtra("entry2"));
+            te3.setText(intent.getStringExtra("entry3"));
+
+        }
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.confirm:
+
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,"http://agni.iitd.ernet.in/cop290/assign0/register/",
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(confirmation.this,response,Toast.LENGTH_LONG).show();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(confirmation.this,error.toString(),Toast.LENGTH_LONG).show();
+                            }
+                        }){
+                    @Override
+                    protected Map<String,String> getParams(){
+                        Map<String,String> params = new HashMap<String, String>();
+                        params.put("teamname",team.getText().toString().trim());
+                        params.put("entry1", te1.getText().toString().trim());
+                        params.put("name1",tn1.getText().toString().trim());
+                        params.put("entry2",te2.getText().toString().trim());
+                        params.put("name2",tn2.getText().toString().trim());
+                        params.put("entry3",te3.getText().toString().trim());
+                        params.put("name3",tn3.getText().toString().trim());
+
+                        return params;
+                    }
+
+                };
+
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+                requestQueue.add(stringRequest);
+
+                break;
+
+        }
+    }
+
+
+
+}
+
